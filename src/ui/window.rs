9072711e03106cb9b478cb5b316532efbc21b65e
margooey <234476012+margooey@users.noreply.github.com>
2026-03-17 21:21:14 -0400

    handle cases where the window could be open, but behind other windows

--- src/ui/window.rs
+++ src/ui/window.rs
@@ -1,8 +1,29 @@
-use objc2::{MainThreadMarker, MainThreadOnly, rc::Retained};
-use objc2_app_kit::{NSBackingStoreType, NSWindow, NSWindowStyleMask};
-use objc2_foundation::{NSRect, NSString};
+use objc2::{
+    MainThreadMarker, MainThreadOnly, define_class, msg_send, rc::{Allocated, Retained},
+};
+use objc2_app_kit::{NSApplication, NSBackingStoreType, NSWindow, NSWindowStyleMask};
+use objc2_foundation::{MainThreadMarker as FMTMarker, NSObject, NSObjectProtocol, NSRect, NSString};
 
 use crate::ui::view::View;
+
+define_class!(
+    #[unsafe(super(NSWindow))]
+    #[name = "LapsusWindow"]
+    struct LapsusWindow;
+
+    // Force the app to the foreground when either opening the settings menu for the first time
+    // or clicking it a second time to bring it back into focus if it is behind other windows.
+    impl LapsusWindow {
+        #[unsafe(method(makeKeyAndOrderFront:))]
+        fn make_key_and_order_front(&self, sender: Option<&NSObject>) {
+            let _: () = unsafe { msg_send![super(self), makeKeyAndOrderFront: sender] };
+            let mtm = FMTMarker::new().expect("must be on the main thread");
+            NSApplication::sharedApplication(mtm).activate();
+        }
+    }
+
+    unsafe impl NSObjectProtocol for LapsusWindow {}
+);
 
 pub struct Window {
     pub window: Retained<NSWindow>,
@@ -16,16 +37,17 @@ impl Window {
         backing_store_type: NSBackingStoreType,
         flag: bool,
     ) -> Self {
-        let window = unsafe {
-            NSWindow::initWithContentRect_styleMask_backing_defer(
-                NSWindow::alloc(mtm),
-                frame,
-                style,
-                backing_store_type,
-                flag,
-            )
+        let alloc: Allocated<LapsusWindow> = LapsusWindow::alloc(mtm);
+        let window: Retained<LapsusWindow> = unsafe {
+            msg_send![
+                alloc,
+                initWithContentRect: frame,
+                styleMask: style,
+                backing: backing_store_type,
+                defer: flag,
+            ]
         };
-        Self { window }
+        Self { window: window.into_super() }
     }
 
     pub fn set_title(&self, string: &str) {
